076726581fb1bf22f7ca9fdd7cc5d1230a2cb5ad
Juliana <juliana.fulanetto@aluno.senai.br>
2025-05-21 10:42:10 -0300

    Ajustes app

---
+++ projeto/src/App.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react"; 
+import { BrowserRouter, Routes, Route } from "react-router"; 
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; 
+import "./styles/main.scss"; 
+
+// importando componentes
+import Navbar from "./components/NavBar"; 
+import Footer from "./components/Footer"; 
+import Home from "./pages/Home"; 
+import Customize from "./pages/Customize"; 
+import Perfil from "./pages/Perfil"; 
+import Carrinho from "./pages/Carrinho"; 
+import Login from "./pages/Login"; 
+import Registro from "./pages/Registro"; 
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import PaymentSuccess from "./pages/PaymentSuccess"; 
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // verificando se o usuário está logado
+  const [cart, setCart] = useState([]); // armazenando os itens do carrinho
+
+  const handleLogin = () => {
+    setIsLoggedIn(true); // definindo o estado de login como verdadeiro
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false); // como falso
+  };
+
+  const addToCart = (item) => {
+    setCart([...cart, item]); // add um item ao carrinho
+  };
+
+  return (
+    <div className="app-container d-flex flex-column min-vh-100"> 
+      <BrowserRouter> {/* fornece o contexto de roteamento */}
+        <Navbar
+          isLoggedIn={isLoggedIn} // estado de login
+          handleLogout={handleLogout} // função de logout
+          cartItems={cart.length} // número de itens no carrinho
+        />
+        <main className="flex-grow-1"> 
+          <Routes> 
+            <Route path="/" element={<Home />} /> {/* rota página inicial */}
+            <Route
+              path="/customize"
+              element={<Customize addToCart={addToCart} />} // rota página de personalização com função de adicionar ao carrinho
+            />
+            <Route
+              path="/login"
+              element={
+                <Login handleLogin={handleLogin} isLoggedIn={isLoggedIn} /> // rota p login 
+              }
+            />
+            <Route
+              path="/registro"
+              element={
+                <Registro handleLogin={handleLogin} isLoggedIn={isLoggedIn} /> // rota p registro com função de login
+              }
+            />
+            <Route
+              path="/perfil"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}> {/* protege a rota do perfil */}
+                <Perfil /> 
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/carrinho"
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}> {/* protege a rota do carrinho */}
+                  <Carrinho cart={cart} setCart={setCart} /> 
+                </ProtectedRoute>
+              }
+            />
+            <Route 
+              path="/payment-success" 
+              element={
+                <ProtectedRoute isLoggedIn={isLoggedIn}> {/* protege a rota de sucesso de pagamento */}
+                  <PaymentSuccess /> 
+                </ProtectedRoute>
+              } 
+            />
+          </Routes>
+        </main>
+        <Footer /> 
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App; 

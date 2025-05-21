37146e6419a3d89673d814fec0a2f7e112ee32b6
Aluna.Lorena <lorennasenaia@gmail.com>
2025-05-21 16:52:09 -0300

    Ajuste footer

--- projeto/src/components/Footer.jsx
+++ projeto/src/components/Footer.jsx
@@ -11,29 +11,29 @@ const Footer = () => {
           <li className="list-inline-item d-flex align-items-center">
             <hr className="d-none d-md-inline-block mx-4" style={{ width: '700px' }} />
             <a href="https://www.youtube.com/@Steam" target="_blank" rel="noopener noreferrer">
-              <img src="/youtube.svg" alt="YouTube" width="24" height="24" style={{ filter: 'invert(0.8)' }} />
+              <img src="/youtube.svg" alt="YouTube" width="24" height="24"  />
             </a>
           </li>
           <li className="list-inline-item d-flex align-items-center">
             <a href="https://www.instagram.com/steam__tr/" target="_blank" rel="noopener noreferrer">
-              <img src="/instagram.svg" alt="Instagram" width="24" height="24" style={{ filter: 'invert(0.8)' }} />
+              <img src="/instagram.svg" alt="Instagram" width="24" height="24"  />
             </a>
           </li>
           <li className="list-inline-item d-flex align-items-center">
             <a href="https://github.com/strwmay/devSteam" target="_blank" rel="noopener noreferrer">
-              <img src="/github.svg" alt="GitHub" width="24" height="24" style={{ filter: 'invert(0.8)' }} />
+              <img src="/github.svg" alt="GitHub" width="24" height="24"  />
             </a>
           </li>
           <li className="list-inline-item d-flex align-items-center">
             <a href="https://www.figma.com/design/hw3MevGX78GTBJzRzEpISe/Projeto-1---Fernanda?node-id=0-1&t=EudwILF32W1YNhLp-1" target="_blank" rel="noopener noreferrer">
-              <img src="/figma.svg" alt="Figma" width="24" height="24" style={{ filter: 'invert(0.8)' }} />
+              <img src="/figma.svg" alt="Figma" width="24" height="24"  />
             </a>
             <hr className="d-none d-md-inline-block mx-4" style={{ width: '700px' }} />
           </li>
         </ul>
         {/* texto de copyright centralizado */}
         <div className="pt-4 w-100 d-flex flex-column align-items-center">
-          <p className="mt-2"><span>Copyright © 2025 DevSteamSenai</span></p>
+          <p className="mt-2"><span>Copyright © 2025 Plushing</span></p>
         </div>
       </div>
     </footer>
